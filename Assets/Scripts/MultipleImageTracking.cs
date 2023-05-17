d4089429acf096023c7ee09e370c6f9e5ead9eb1
FelForest <fel.forest3910@gmail.com>
2023-05-17 22:35:29 +0900

    이미지 중앙 트래킹

---
+++ Assets/Scripts/MultipleImageTracking.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class MultipleImageTracking : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] Objs;
+
+    private Dictionary<string, GameObject> spawnedObjs = new Dictionary<string, GameObject>();
+    private ARTrackedImageManager ARTrackedImageManager;
+
+    public GameObject blueprint;
+    private void Awake()
+    {
+        ARTrackedImageManager = GetComponent<ARTrackedImageManager>();
+        foreach (GameObject prefab in Objs)
+        {
+            GameObject clone = Instantiate(prefab);
+            spawnedObjs.Add(prefab.name, clone);
+            clone.SetActive(false);
+        }
+    }
+
+    void OnEnable()
+    {
+        ARTrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    void OnDisable()
+    {
+        ARTrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var trackedImage in eventArgs.added)
+        {
+            UpdateImage(trackedImage);
+        }
+
+        foreach (var trackedImage in eventArgs.updated)
+        {
+            UpdateImage(trackedImage);
+        }
+
+        foreach (var trackedImage in eventArgs.removed)
+        {
+            spawnedObjs[trackedImage.name].SetActive(false);
+        }
+    }
+
+    private void UpdateImage(ARTrackedImage trackedImage)
+    {
+        GameObject trackedObject = spawnedObjs[trackedImage.referenceImage.name];
+
+
+        if (trackedImage.trackingState == TrackingState.Tracking)
+        {
+            if(blueprint.transform.position == trackedImage.transform.position)
+            {
+                trackedObject.transform.position = trackedImage.transform.position;
+                trackedObject.transform.rotation = trackedImage.transform.rotation;
+                trackedObject.SetActive(true);
+            }
+        }
+        else
+        {
+            trackedObject.SetActive(false);
+        }
+    }
+}
